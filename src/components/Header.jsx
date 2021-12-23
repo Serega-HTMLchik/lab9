67b2151b9996bc68d9db088367c17149b55d44db
Serega-HTMLchik <70023791+Serega-HTMLchik@users.noreply.github.com>
2021-12-23 21:09:04 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     window.location.reload();
   }
   return (
-    <div  style={{width:'100%', backgroundColor:'#9090e8'}}>
+    <div  style={{width:'100%', backgroundColor:'#9090e8', height:'200px'}}>
       <Link className="btn btn-primary" to="/">
         Main
       </Link>
